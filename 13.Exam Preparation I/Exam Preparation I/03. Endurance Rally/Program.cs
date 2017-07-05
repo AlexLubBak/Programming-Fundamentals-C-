375989bcb501356df5cc7ce15208c14403ccfe8d
Aleksandar Bakardzhiev <compworld@abv.bg>
2017-07-06 02:12:10 +0300

    update

--- 13.Exam Preparation I/Exam Preparation I/03. Endurance Rally/Program.cs
+++ 13.Exam Preparation I/Exam Preparation I/03. Endurance Rally/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _03.Endurance_Rally
@@ -10,8 +11,80 @@ namespace _03.Endurance_Rally
     {
         static void Main(string[] args)
         {
-            string[] all = Regex.Split(Console.ReadLine(), @"\s*,\s*");
+            string[] allDrivers = Regex.Split(Console.ReadLine(), @"\s+")
+                .Where(t => t.Length > 0)
+                .ToArray();
+            double[] allPointsRoute = Regex.Split(Console.ReadLine(), @"\s+")
+                .Where(t=>t.Length>0)
+                .Select(p => double.Parse(p)*-1).ToArray();
+            List<long> fuelFillingPoints = Regex.Split(Console.ReadLine(), @"\s")
+                 .Where(t => t.Length > 0)
+                 .Select(long.Parse)
+                 .Distinct()
+                .ToList();
 
+            long lengthName = allDrivers.Length;
+            long lengthPointRoute = allPointsRoute.Length;
+            long lengthFillingPoints = fuelFillingPoints.Count;
+
+            List<double> startingFuelDriver = new List<double>();
+            long lengthList = startingFuelDriver.Count;
+
+            var index = 0;
+            for (int i = 0; i < lengthName; i++)
+            {
+                string driversName = allDrivers[i];
+
+                for (int j = 0; j < 1; j++)
+                {
+                    double startingFuel = driversName[j];
+                    startingFuelDriver.Add(startingFuel);
+                }
+                
+            }
+
+            foreach (var item in fuelFillingPoints)
+            {
+                if (item >= 0 && item < allPointsRoute.Length)
+                {
+                    allPointsRoute[item] *= -1.0;
+                }
+               
+
+            }
+                
+        double startingFuelDriverTemp = 0.0;
+
+            for (int i = 0; i < startingFuelDriver.Count; i++)
+            {
+                
+                index = 0;
+                for (int j = 0; j < lengthPointRoute; j++)
+                {
+                    
+                    
+                        startingFuelDriver[i] += allPointsRoute[j];
+
+                        if (startingFuelDriver[i]<=0)
+                        {
+                           
+
+                            Console.WriteLine($"{allDrivers[i]} - reached {index}");
+                        break;
+                        }
+                    index++;
+
+                    startingFuelDriverTemp = startingFuelDriver[i];
+                }
+                if (startingFuelDriver[i]>0)
+                {
+                    Console.WriteLine($"{allDrivers[i]} - fuel left {startingFuelDriver[i]:F}");
+                }
+             
+
+            }
+            
+                       
         }
     }
 }
---
+++ 13.Exam Preparation I/Exam Preparation I/04. Winning Ticket/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04.Winning_Ticket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allTicket = Regex.Split(Console.ReadLine(), @"\s*,\s*")
+                .Select(t=>t.Trim())
+               .Where(t => t.Length > 0)
+               .ToArray();
+
+            int numberTicket = allTicket.Length;
+
+            foreach( var t in allTicket)
+            {
+                if (t.Length!=20)
+                {
+                    Console.WriteLine("invalid ticket");
+                }
+                else
+                {
+                    var leftSide =  t.Substring(0, 10);
+
+                    var rightSide = t.Substring(10, 10);
+                    var maxLeftLongest = FindMaxEqualSeq(leftSide);
+                    var maxRightLongest = FindMaxEqualSeq(rightSide);
+
+                    int leftSideLongest = maxLeftLongest.Length;
+                    int rightSideLongest = maxRightLongest.Length;
+
+                    if (maxRightLongest[0] == maxLeftLongest[0] &&
+                        leftSideLongest>=6 && rightSideLongest>=6 &&
+                        "@#$^".Contains(maxLeftLongest[0]))
+                    {
+                       
+
+                        int minLength = Math.Min(leftSideLongest, rightSideLongest);
+
+                        if (minLength == 10)
+                        {
+                            Console.WriteLine($"ticket \"{t}\" - {minLength}{maxLeftLongest[0]} Jackpot!");
+                        }
+                        else
+                        {
+                           
+                                Console.WriteLine($"ticket \"{t}\" - {minLength}{maxLeftLongest[0]}");
+                           
+                               
+                            
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ticket \"{t}\" - no match");
+                    }
+
+                }
+                
+            }
+
+                      
+        }
+
+        static string FindMaxEqualSeq(string s)
+        {
+            var bestStr ="" + s[0];
+            var max = 1;
+
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                var ch = s[i];
+                var count = 1;
+
+                while (i+count < s.Length && s[i+count]== s[i])
+                {
+                    count++;
+
+                    if (count>max)
+                    {
+                        max = count;
+                        bestStr = s.Substring(i, count);
+
+                    }
+
+                }
+
+            }
+            return bestStr;
+
+        }
+    }
+}
